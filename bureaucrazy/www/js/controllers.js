6fc851963cb84894c52e447d7e192320d0a97b35
Robert Willemelis <r.wille3055@gmx.de>
2016-06-05 05:25:32 +0200

    feat(view): addint 3rd view
    Closes #4

--- bureaucrazy/www/js/controllers.js
+++ bureaucrazy/www/js/controllers.js
@@ -5,3 +5,20 @@ angular.module('starter.controllers', [])
 
 .controller('OnBoardingCtrl', function($scope) {
 });
+
+.controller('TopicsCtrl', function($scope, Chats, $ionicNavBarDelegate) {
+  //$ionicNavBarDelegate.showBackButton([true]);
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+
+  // not working on back
+  $scope.$on('$ionicView.loaded', function(e) {
+    $ionicNavBarDelegate.title('The list of topics');
+  });
+
+
+  $scope.chats = Chats.all();
+})
